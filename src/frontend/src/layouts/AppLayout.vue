bfcbf4df959924310ef024e0627a1a9c0bf17a6d
Eugene <evg-nikitin@mail.ru>
2022-08-08 23:53:21 +0300

    module6-task1

--- src/frontend/src/layouts/AppLayout.vue
+++ src/frontend/src/layouts/AppLayout.vue
@@ -18,19 +18,3 @@ export default {
   },
 };
 </script>
-<style>
-.slither-enter-active,
-.slither-leave-active {
-  transition: transform 1s;
-}
-
-.slither-enter,
-.slither-leave-to {
-  transform: translateX(-100%);
-}
-
-.slither-enter-to,
-.slither-leave {
-  transform: translateX(0);
-}
-</style>
